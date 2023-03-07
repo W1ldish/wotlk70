f7f731c11740fd731c80f211ae325a6f53929556
W1ldish <124502567+W1ldish@users.noreply.github.com>
2023-03-07 20:38:10 +0100

    Update melee_trinkets.go

--- sim/common/tbc/melee_trinkets.go
+++ sim/common/tbc/melee_trinkets.go
@@ -36,7 +36,7 @@ func init() {
 	core.NewSimpleStatOffensiveTrinketEffect(23041, stats.Stats{stats.AttackPower: 260, stats.RangedAttackPower: 260}, time.Second*20, time.Minute*2)  // Slayer's Crest
 	core.NewSimpleStatOffensiveTrinketEffect(24128, stats.Stats{stats.AttackPower: 320, stats.RangedAttackPower: 320}, time.Second*12, time.Minute*3)  // Figurine Nightseye Panther
 	core.NewSimpleStatOffensiveTrinketEffect(28041, stats.Stats{stats.AttackPower: 200, stats.RangedAttackPower: 200}, time.Second*15, time.Minute*2)  // Bladefists Breadth
-	core.NewSimpleStatOffensiveTrinketEffect(28121, stats.Stats{stats.ArmorPenetration: 600}, time.Second*20, time.Minute*2)                           // Icon of Unyielding Courage
+	core.NewSimpleStatOffensiveTrinketEffect(28121, stats.Stats{stats.ArmorPenetration: 85}, time.Second*20, time.Minute*2)                           // Icon of Unyielding Courage
 	core.NewSimpleStatOffensiveTrinketEffect(28288, stats.Stats{stats.MeleeHaste: 260}, time.Second*10, time.Minute*2)                                 // Abacus of Violent Odds
 	core.NewSimpleStatOffensiveTrinketEffect(29383, stats.Stats{stats.AttackPower: 278, stats.RangedAttackPower: 278}, time.Second*20, time.Minute*2)  // Bloodlust Brooch
 	core.NewSimpleStatOffensiveTrinketEffect(29776, stats.Stats{stats.AttackPower: 200, stats.RangedAttackPower: 200}, time.Second*20, time.Minute*2)  // Core of Arkelos
